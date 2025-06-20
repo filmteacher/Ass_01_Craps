2832005a99ab0767b89bbb6558b64083a663a983
Matt Bennett <144359294+filmteacher@users.noreply.github.com>
2025-06-19 21:55:02 -0400

    Completed game

--- src/Main.java
+++ src/Main.java
@@ -26,29 +26,29 @@ public class Main
                 die2 = rnd.nextInt(6) + 1;
                 sum = die1 + die2;
                 //For each roll show the value for each die and the sum.
-                System.out.print("Die 1 is: ", die1, ". Die 2 is: ", die2, ". The sum is: ", sum,".");
+                System.out.printf("\nDie 1 is: %d. Die 2 is: %d. The sum is: %d.\n", die1, die2, sum);
 
                 //If the sum is 2, 3 or 12 it is called "craps" or "crapping out" and the game is over with a loss.
                 //o	For case i or ii above, indicate that the user either crapped out or won with a natural and then prompt to play again
-                if (sum == 2) || (sum == 3) || (sum == 12):
+                if (sum == 2 || sum == 3 || sum == 12)
                 {
-                    System.out.print("You crapped out. Game lost.");
+                    System.out.print("You crapped out. Game lost.\n");
                     gameOver = true;
                 }
                 //If the sum is 7 or 11 it is called a ‘natural’ and the game is over with a win.
                 //o	For case i or ii above, indicate that the user either crapped out or won with a natural and then prompt to play again
-                else if (sum == 7) || (sum == 11):
+                else if (sum == 7 || sum == 11)
                 {
-                    System.out.print("You rolled a natural. You win!");
+                    System.out.print("You rolled a natural. You win!\n");
                     gameOver = true;
                 }
                 //For all other values, the sum becomes ‘the point’ and the user makes subsequent rolls until they either throw a 7 in which case they lose or they make the point sum in which case they win.
-                else:
+                else
                 {
                     point = sum;
                     rollPoint = true;
                     //o	For case iii indicate that the sum is now the point.
-                    System.out.print("The point is: ", point, ". You are trying for the point.");
+                    System.out.printf("The point is: %d. You are trying for the point.\n", point);
 
                     do
                     {
@@ -56,27 +56,27 @@ public class Main
                         die2 = rnd.nextInt(6) + 1;
                         sum = die1 + die2;
 
-                        if (die1 == 7) || (die2 == 7):
+                        if (die1 == 7 || die2 == 7)
                         {
                             // Got a seven and lost   (Prompt user to play again.)
-                            System.out.print("Die 1 is: ", die1, ". Die 2 is: ", die2, ". You rolled a 7. Game lost.");
+                            System.out.printf("Die 1 is: d%. Die 2 is: d%. You rolled a 7. Game lost.\n", die1, die2);
                             gameOver = true;
                             rollPoint = false;
                         }
-                        else if (sum == point):
+                        else if (sum == point)
                         {
                             // Made point and won.  (Prompt user to play again.)
-                            System.out.print("Die 1 is: ", die1, ". Die 2 is: ", die2, ". The sum is: ", sum,". You made the point. You win!");
+                            System.out.printf("Die 1 is: %d. Die 2 is: %d. The sum is: %d. You made the point. You win!\n", die1, die2, sum);
                             gameOver = true;
                             rollPoint = false;
                         }
-                        else:
+                        else
                         {
                             //Trying for point
-                            System.out.print("Die 1 is: ", die1, ". Die 2 is: ", die2, ". The sum is: ", sum,". You did not make the point. Shoot again.");
+                            System.out.printf("Die 1 is: %d. Die 2 is: %d. The sum is: %d. You did not make the point. Shoot again.\n", die1, die2, sum);
                         }
                     }
-                    while (rollPoint)
+                    while (rollPoint);
                 }
             }
             while(!gameOver);
